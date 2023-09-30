9c33da86a3cd25694864c998a5f4a2078aefcf0b
Vinicius <viniciusmartins-@live.com>
2023-09-30 17:19:37 -0300

    first commmit

---
+++ src/main/java/com/github/viniciusmartins/testcontainerconfig/TestcontainerconfigApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciusmartins.testcontainerconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainerconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainerconfigApplication.class, args);
+	}
+
+}
